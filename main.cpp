d10783dd3209a88b3e4345db0754162be8f8a1c5
GregoryMason <GregoryMason@users.noreply.github.com>
2017-05-11 10:56:43 +1200

    So-far complete main.cpp
    Full Main code for the robot

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include "E101.h"
+
+int numPixels = 0;
+int linePlace[32];
+
 void setMotors(int offSet){
 	int left_motor = 1;
 	int right_motor = 2;
@@ -19,10 +25,67 @@ void setMotors(int offSet){
 	}
 }
 
+/*Returns the average values of the pixels of 3 rows
+ *(one row above, and one row below the passed row int)
+ *int row must be between 1 and 238
+ */
+void getRow(int row){
+	int rowsAv[32];
+	int pixelTotal = 0; 	//Total value of the pixels from all 3 rows
+	int average; 	    	// will hold the average value of the three pixels
+	int max = 200;
+	
+	int i = 0;
+	for (int pixel = 0; pixel < 320; pixel += 10) { //Get's every 4th pixel
+		pixelTotal = get_pixel(row-1,pixel,3) + get_pixel(row,pixel,3) + get_pixel(row+1,pixel,3);
+		average = pixelTotal/3;
+		rowsAv[i++] = average;
+		if (average > max) max = average;
+	}
+	
+	int threshold = max / 2;
+	
+	printf("[ ");
+	for (int pixel = 0; pixel < 32; pixel++) {
+		if (rowsAv[pixel] > threshold) {
+			linePlace[pixel] = 1;
+		} else {
+			linePlace[pixel] = 0;
+		}
+		printf(linePlace[pixel]);
+	}
+	printf("]\n");
+}
+
+/*Detects where the white line is in relation to the centre of the robot.
+ *returns positive integer if line is to the right
+ *returns negative integer if line is to the left
+ */
+double detectLine(){
+	//take a picture
+	take_picture();
+	display_picture(1,0);
+	
+	getRow(160);
+	
+	double error = 0;
+	numPixels = 0;
+	for (int pixel = 0; pixel < 32; pixel++) {
+		if (linePlace[pixel] == 1){
+			error += (pixel-16);
+			numPixels++;
+		}
+	}
+	if (numPixels > 0) {
+		return (error/numPixels);
+	} else {
+		return 0;
+	}
+}
+
 int main(){
 	init();
-	int numPixels = 0;
-	
+		
 	while (true){
 		setMotors(detectLine());
 		if (numPixels > 24){
