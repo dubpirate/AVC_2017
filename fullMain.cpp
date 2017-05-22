5ff05311e2c855859f8ad6340fc82b267a844827
GregoryMason <GregoryMason@users.noreply.github.com>
2017-05-22 16:22:44 +1200

    Now more maintainable! With less redundancy
    Added in a whole bunch of comments to help with understanding what's happening in the code, along the lines of maintainability. Also went through and removed some commented out print statements, and other now-redundant code.

--- fullMain.cpp
+++ fullMain.cpp
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include "E101.h"
-#include <stdlib.h>
 
-int numPixels = 0;
-int linePlace[320] = {0};
+int numPixels = 0;          //Number of white pixels in the row
+int linePlace[320] = {0};   //Array storing detection of line for pixels
 
+/**Sets the motors of the robot to turn towards where it should be
+ * @param offSet how far off centre the desired position is
+ */
 void setMotors(int offSet){
 	int left_motor = 1;
 	int right_motor = 2;
-	int speedFact = 40;
-	//double scaleFact = 1.0;
-	//offSet = abs(offSet);
-	double prop = 0.002;//((double)offSet/136);
-	int turnFact = offSet * prop;
-	if (turnFact > (255-speedFact)) {
-		turnFact = (255 - speedFact);
-	}
 
-	printf("%d\n", offSet);
+	int speedFact = 40;     //How fast the robot moves by default
+	double prop = 0.002;    //the proportional value for the offset
+	int turnFact = offSet * prop;   //How sharply the robot turns to the left/right
+
+    //Limit the turnFact, so it doesn't end up too large for set_motor
+	if (turnFact > (254-speedFact)) turnFact = (254 - speedFact);
+
 	if (offSet > 0) { //Line is to the right
 		set_motor(left_motor, -1*(speedFact + (int)(offSet * prop)));
 		set_motor(right_motor, speedFact - (int)(offSet * prop));
@@ -25,7 +25,7 @@ void setMotors(int offSet){
 		set_motor(left_motor, -1*(speedFact + (int)(offSet * prop)));
 		set_motor(right_motor,(speedFact - (int)(offSet * prop)));
 	}else if (offSet == 0) {
-		if (numPixels == 0) {
+		if (numPixels == 0) { //If line isn't detected, then reverse, slightly offset
 			set_motor(left_motor, speedFact);
 			set_motor(right_motor, -speedFact-10);
 		} else {
@@ -35,87 +35,80 @@ void setMotors(int offSet){
 	}
 }
 
-/*Returns the average values of the pixels of 3 rows
- *(one row above, and one row below the passed row int)
- *int row must be between 1 and 238
+/**Sets the values in linePlace[] for if it's white or black
+ * @param row The row of the image which is read
+ * (Takes the average of the 3 rows centered on passed row)
  */
 void getRow(int row){
 	int rowsAv[320] = {0};
 	int pixelTotal = 0; 	//Total value of the pixels from all 3 rows
-	int average = 0; 	    	// will hold the average value of the three pixels
-	int max = 150;
-	
-	int i = 0;
-	for (int pixel = 0; pixel < 320; pixel++ ) { //Get's every 10th pixel
-		//printf("%d\n", pixel);
+	int average = 0; 	    //The average (mean) value of the three pixels
+	int max = 150;          //Default maximum value, acts as the minimum for the threshold
+
+    if (row > 238) row = 238;   //Guarantee that the value passed is next too big
+    if (row < 1) row = 1;       //or that the value is not too small
+
+	for (int pixel = 0; pixel < 320; pixel++ ) { //Get's every pixel
 		pixelTotal = get_pixel(row-1,pixel,3) + get_pixel(row,pixel,3) + get_pixel(row+1,pixel,3);
-		average = pixelTotal/3;
-		//printf("%d\n", average);
-		rowsAv[i++] = average;
-		//printf("%d\n", rowsAv[i++]);
-		if (average > max) max = average;
+		average = pixelTotal/3; //Get the int average of the 3 pixels
+		rowsAv[pixel] = average;
+		if (average > max) max = average; //Make the threshold higher if the line brighter
 	}
-	//printf("End\n");
 	
 	int threshold = max / 2;
-	
-	//printf("[ ");
-	for (int pixel = 0; pixel < 320; pixel++) {
-		//printf("%d\n", pixel);
-		if (rowsAv[pixel] > threshold) {
-			linePlace[pixel] = 1;
+
+	for (int pixel = 0; pixel < 320; pixel++) {//Iterate through the row of pixels
+		if (rowsAv[pixel] > threshold) {    //Test if pixel value is above threshold (white)
+			linePlace[pixel] = 1;           //If so, set it to show there's a white pixel there (1)
 		} else {
-			linePlace[pixel] = 0;
+			linePlace[pixel] = 0;           //Otherwise, there is no white pixel there (0)
 		}
-	//	printf("%d ", linePlace[pixel]);
 	}
-	//printf("]\n");
 }
 
-/*Detects where the white line is in relation to the centre of the robot.
- *returns positive integer if line is to the right
- *returns negative integer if line is to the left
- */
+ /**Detects placement of white line in relation to centre of image
+  * @return proportional value of where the line is, relative to centre
+  * Value is positive if to the right, negative if to the left
+  */
 double detectLine(){
-	//take a picture
-	take_picture();
-	//display_picture(1,0);
+	take_picture(); //Take a picture with the camera
+	getRow(160);    //and read the pixels in line 160 to find the line
 	
-	getRow(160);
-	
-	double error = 0;
-	numPixels = 0;
+	double error = 0;   //The error signal
+	numPixels = 0;      //reset the number of white pixels
 	for (int pixel = 0; pixel < 320; pixel++) {
 		if (linePlace[pixel] == 1) {
-			error += (pixel-160);
-			numPixels++;
+			error += (pixel-160); //Increase error signal, according to distance from centre
+			numPixels++;          //Increment the number of how many white pixels there are
 		}
 	}
-	
-	return (error);
-	}
+
+    return (error);
+}
 
 int main(){
-	init();
+	init(); //Initialise the Raspberry Pi
 	
-	char serverAdd[15] = {0};
+	//Set the address of the server for the gate
+    char serverAdd[15] = {0};
 	sprintf(serverAdd, "130.195.6.196");
 	int serverPort = 1024;
-	char serverReply[24] = {0};
-	char serverMessage[24] = {0};
+
+    //Set the message to send to the server, and initialise the reply
+	char serverMessage[24] = {''};
 	sprintf(serverMessage, "Please");
-	
+    char serverReply[24] = {''};
+
+    //Connect to server, then send/receive message for opening the gate
 	connect_to_server(serverAdd, serverPort);
 	send_to_server(serverMessage);
 	receive_from_server(serverReply);
 	send_to_server(serverReply);
-	setMotors(detectLine());
+
+	setMotors(detectLine()); //Start moving the robot
 	
-	while (true){
-		setMotors(detectLine());
-		//if (numPixels > 24){
-		//	break;
-		//}
+	while (true){ //Loop endlessly
+		setMotors(detectLine()); //Move robot according to where the line is
 	}
-	stop(0);
+	stop(0); //Stop the robot
 return 0;}
